c53edca138732f4e385691818b746ee08c0d0ec7
Jiaqi Li <jiaqi.li@lungteh.com>
2025-10-08 11:14:09 +0800

    Implement validate email.

--- main.js
+++ main.js
@@ -1,14 +1,28 @@
 const submitButton = document.getElementById('submitbtn');
 const dismissButton = document.getElementById('dismissbtn');
+const emailInput = document.getElementById('email');
+const emailError = document.getElementById('email-error');
+
+document.getElementById('email').addEventListener('input', function() {
+  setDefaultStyles();
+});
 
 document.getElementById('submitbtn').addEventListener('click', function() {
-  SwitchLayout(false);
-});
-document.getElementById('dismissbtn').addEventListener('click', function() {
-  SwitchLayout(true);
+  if (!validateEmail(emailInput.value)) {
+    setErrorStyles();
+    return; // Stop further execution if email is invalid
+  } else {
+    setDefaultStyles();
+    switchLayout(false);
+  }
 });
 
-function SwitchLayout(boolean) {
+document.getElementById('dismissbtn').addEventListener('click', function() {
+  emailInput.value = ''; // Clear the input field
+  switchLayout(true);
+});
+
+function switchLayout(boolean) {
   if (boolean) {
     document.querySelector('.container').style.display = 'flex';
     document.querySelector('.success-message').style.display = 'none';
@@ -17,4 +31,23 @@ function SwitchLayout(boolean) {
     document.querySelector('.container').style.display = 'none';
     document.querySelector('.success-message').style.display = 'flex';
   }
+}
+
+function validateEmail(email) {
+  const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return re.test(String(email).toLowerCase());
+}
+
+function setDefaultStyles() {
+  emailError.style.display = '';
+  emailInput.style.color = 'var(--Blue800)';
+  emailInput.style.border = '1px solid var(--Grey)';
+  emailInput.classList.remove('input-error');
+}
+
+function setErrorStyles() {
+  emailError.style.display = 'block';
+  emailInput.style.color = 'var(--Red)';
+  emailInput.style.border = '1px solid var(--Red)';
+  emailInput.classList.add('input-error');
 }
